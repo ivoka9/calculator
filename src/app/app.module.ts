8eff9bacc2ee018daec11639de4ab0dfa97b35b8
ivo <ivailo96g@gmail.com>
2023-11-01 08:01:55 -0400

    [Feat] Project created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ScreenComponent } from './components/screen/screen.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScreenComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule
