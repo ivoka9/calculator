7aab0fb555c88abe862ac3d1aa2c321d7ceca557
ivo <ivailo96g@gmail.com>
2023-11-03 07:30:50 -0400

    [Fix] Typo

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const app = require("./app");
 
 const port = 8000;
-console.log("Server Running On" + port);
+console.log("Server Running On " + port);
 
 const server = http.createServer(app);
 
